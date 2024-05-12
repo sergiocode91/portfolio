223493dd9bf3c428e4f1c5e07d8ae954084be7aa
sergiodev91 <sergiodev91@gmail.com>
2024-05-11 22:05:37 -0400

    ✨ [ADD] Update views

--- src/pages/api/[id].json.ts
+++ src/pages/api/[id].json.ts
@@ -12,21 +12,28 @@ export const GET: APIRoute = async ({ params }) => {
     return new Response(JSON.stringify({ error: error.message }), { status: 500 });
   }
 
+  const { error: updateError } = await supabase
+    .from('post_views')
+    .update({ views_count: data.views_count + 1 })
+    .eq('post_id', params.id);
+
+  if (updateError) {
+    return new Response(JSON.stringify({ error: updateError.message }), { status: 500 });
+  }
+
   return new Response(JSON.stringify(data), { headers: { 'Content-Type': 'application/json' } });
 };
 
 export async function getStaticPaths() {
-  const { data, error } = await supabase
-    .from('post_views')
-    .select("post_id");
+  const { data, error } = await supabase.from('post_views').select('post_id');
 
   if (error) {
-    console.error("Error fetching slugs:", error);
+    console.error("Error fetching post ids:", error);
     return [];
   }
 
   const paths = data.map(post => ({
-    params: { id: post.post_id }
+    params: { id: String(post.post_id) }
   }));
 
   return paths;
